c8cc365bfa45af72a79290bf1b8e1712ec5ad352
Patricia99 <patri.basme.99@gmail.com>
2023-02-02 09:55:17 +0100

    Create toggle.js

---
+++ js/toggle.js
@@ -0,0 +1,9 @@
+var toggle = document.getElementById("container-toggle");
+var historia = document.getElementById("historia_oscuro");
+var logo = document.getElementById("logo_historia");
+
+toggle.onclick = function() {
+    toggle.classList.toggle("active");
+    historia.classList.toggle("active");
+    logo.classList.toggle("active");
+}
