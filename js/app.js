727de8dfc1063269985fdde74306922b4de47475
Patricia99 <patri.basme.99@gmail.com>
2023-02-02 09:55:11 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var header = document.getElementById('menu');
+var header_historia = document.getElementById("menu_historia");
 
 window.addEventListener('scroll', ()=>{
     var scroll = window.scrollY;
@@ -6,5 +7,15 @@ window.addEventListener('scroll', ()=>{
         header.style.backgroundColor = '#161616';
     } else {
         header.style.backgroundColor = 'transparent';
+    }
+})
+
+window.addEventListener('scroll', ()=>{
+    var scroll = window.scrollY;
+    if (scroll > 10) {
+        header_historia.style.backgroundColor = '#161616';
+        header_historia.style.color = '#fff';
+    } else {
+        header_historia.style.backgroundColor = 'transparent';
     }
 })
